05114e35dd220d494c310d27140397e8357a2dd1
209450 <209450@edu.p.lodz.pl>
2019-06-17 22:48:01 +0200

    Testy parametryczne dla test klasy ServerLoadBalancerParametrizedTest

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
@@ -4,19 +4,44 @@ import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoa
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest{
-	@Test
-	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
-		Server theServer = a(server().withCapacity(1));
-		Vm theVm = a(vm().ofSize(1));
-		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
-		assertThat(theServer, hasLoadPercentageOf(100.0d));
-		assertThat("the server should contain vm", theServer.contains(theVm));
-	}
-	
-	
+@RunWith(value = Parameterized.class)
+public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest {
+
+    @Parameterized.Parameters public static Collection<Object[]> defaultDataToTest() {
+        return Arrays.asList(
+                new Object[][] {{0, 0, 0d, false}, {0, 1, 0d, false}, {1, 0, 0d, true}, {1, 1, 100.0d, true}, {1, 2, 0d, false}, {2, 1, 50.0d, true}, {2, 2, 100.0d, true}});
+    }
+
+    @Parameterized.Parameter(value = 0) public int inputServerCapacity;
+
+    @Parameterized.Parameter(value = 1) public int inputVmSize;
+
+    @Parameterized.Parameter(value = 2) public double expectedLoadPercentage;
+
+    @Parameterized.Parameter(value = 3) public boolean expectedServerContainsVm;
+
+    @Test public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
+        Server theServer = a(server().withCapacity(inputServerCapacity));
+        Vm theVm = a(vm().ofSize(inputVmSize));
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+        assertThat(theServer, hasLoadPercentageOf(expectedLoadPercentage));
+        assertThat(theServer.contains(theVm), is(expectedServerContainsVm));
+    }
+
+
+
 }
